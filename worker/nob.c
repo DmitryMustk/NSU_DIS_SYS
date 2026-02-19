2ab17a9271a0d297d9a389072dc37ae08872d681
DmitryMustk <dmutrymust@gmail.com>
2026-02-20 02:36:20 +0700

    build fix

--- worker/nob.c
+++ worker/nob.c
@@ -3,20 +3,21 @@
 
 #include "./thirdparty/nob.h"
 
-#define BUILD_FOLDER "bin/"
-#define SRC_FOLDER   "src/"
+#define BUILD_FOLDER   "bin/"
+#define INCLUDE_FOLDER "include/"
+#define SRC_FOLDER     "src/"
 
 #define EXIT_FAILURE 1
 #define EXIT_SUCCESS 0
 
 int main(int argc, char** argv) {
 	NOB_GO_REBUILD_URSELF(argc, argv);
-	if (!mkdir_if_not_exists(BUILD_FOLDER)) {
+	if (!mkdir_if_not_exists(BUILD_FOLDER) || !mkdir_if_not_exists(INCLUDE_FOLDER)) {
 		return EXIT_FAILURE;
 	}
 
 	Cmd cmd = {0};
-
+	
 	if (needs_rebuild1("./include/instpow.h", "./instpow.py")) {
 		cmd_append(&cmd, "python3", "instpow.py");
 		if (!cmd_run(&cmd)) {
